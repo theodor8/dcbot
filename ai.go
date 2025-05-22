c6d120347933e555d706f27997bd94dc2216624a
Theodor <theodju@gmail.com>
2025-05-22 23:20:50 +0200

    streaming responses

--- ai.go
+++ ai.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
@@ -10,7 +11,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var systemMessage = `Du är en discord bot. Du ska svara på frågor och ge information om olika ämnen. Du ska svara kort och koncist. Svara alltid i Discords markdown-format. Använd följande syntax när det är relevant:
+var systemMessage = `Du är en discord bot. Du ska svara på frågor och ge information om olika ämnen. Du ska svara kort och koncist. Du formaterar alltid svar för att se tydliga och välstrukturerade ut i Discord. Använd markdown för att göra texten lättläst, men håll det enkelt och undvik komplex formatering som kan se konstigt ut i Discord. Prioritetera läsbarhet. Använd kortfattade svar med tydliga rubriker, punktlistor och kodblock endast när det behövs. Anpassa alltid för Discord:
 
 - ` + "`**fetstil**`" + ` för betoning
 - ` + "`*kursiv*`" + ` för subtil betoning eller termer
@@ -77,18 +78,63 @@ var params = openai.ChatCompletionNewParams{
 	},
 }
 
-func createCompletion(s *discordgo.Session, m *discordgo.MessageCreate) {
-	completion, err := client.Chat.Completions.New(context.TODO(), params)
-	if err != nil {
-		log.Error("Error calling AI API: ", err)
-		return
+func startStreaming(s *discordgo.Session, m *discordgo.MessageCreate) openai.ChatCompletionAccumulator {
+	stream := client.Chat.Completions.NewStreaming(context.TODO(), params)
+	acc := openai.ChatCompletionAccumulator{}
+	var message *discordgo.Message = nil
+	var ticker *time.Ticker = nil
+	done := make(chan bool)
+	for stream.Next() {
+		chunk := stream.Current()
+		if !acc.AddChunk(chunk) {
+			log.Error("Error adding chunk: ", stream.Err())
+		}
+
+		if len(chunk.Choices) == 0 || chunk.Choices[0].Delta.Content == "" || message != nil {
+			continue
+		}
+
+		var err error
+		message, err = s.ChannelMessageSend(m.ChannelID, acc.Choices[0].Message.Content)
+		if err != nil {
+			log.Error("Error sending message: ", err)
+		}
+		ticker = time.NewTicker(400 * time.Millisecond)
+		go func() {
+			log.Info("Ticker started")
+			editMessage := func() {
+				message, err = s.ChannelMessageEdit(m.ChannelID, message.ID, acc.Choices[0].Message.Content)
+				if err != nil {
+					log.Error("Error editing message: ", err)
+				}
+			}
+			for {
+				select {
+				case <-done:
+					editMessage()
+					ticker.Stop()
+					log.Info("Ticker stopped")
+					return
+				case <-ticker.C:
+					editMessage()
+				}
+			}
+		}()
+
 	}
+	if ticker != nil {
+		done <- true
+	}
+	if stream.Err() != nil {
+		log.Error("Error streaming completion: ", stream.Err())
+	}
+	return acc
+}
+
+func createCompletion(s *discordgo.Session, m *discordgo.MessageCreate) {
+	completion := startStreaming(s, m)
 
 	log.Infof("Prompt tokens: %v, completion tokens: %v", completion.Usage.PromptTokens, completion.Usage.CompletionTokens)
-
-	if len(completion.Choices) != 1 {
-		log.Error("Error: expected 1 choice, got ", len(completion.Choices))
-	}
 
 	params.Messages = append(params.Messages, completion.Choices[0].Message.ToParam())
 
@@ -97,6 +143,4 @@ func createCompletion(s *discordgo.Session, m *discordgo.MessageCreate) {
 		toolCallsHandler(toolCalls, s, m)
 		return
 	}
-
-	s.ChannelMessageSend(m.ChannelID, completion.Choices[0].Message.Content)
 }
